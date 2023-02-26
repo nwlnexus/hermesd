cb44059b12164eb23f5021acbf249518da60a81a
Nigel Williams-Lucas <yesniah@gmail.com>
2023-02-25 22:12:05 -0500

    Working through CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
-mod cli;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author = "NWLNEXUS LLC", version, about)]
+/// Exports designated system properties to the upstream service.
+struct Cli {
+    #[command(subcommand)]
+    sub_cmds: SubCmds,
+}
+
+#[derive(Subcommand, Debug)]
+enum SubCmds {
+    #[command(arg_required_else_help(true))]
+    /// Manages the cloudflared service.
+    Service {},
+    #[command(arg_required_else_help(true))]
+    /// Runs the application and begins reporting metrics.
+    Run {
+        #[arg(short, long)]
+        token: String,
+    },
+}
 
 fn main() {
-    println!("Hello world.")
+    let args = Cli::parse();
+
+    println!("You chose: {:?}", args);
 }
